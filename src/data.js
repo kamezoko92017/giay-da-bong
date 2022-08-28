10ec2861ced8e8d62dc1a610307598b4e5a2f561
albertbrasi <albertbrasi001@gmail.com>
2022-08-28 21:11:32 +0700

    Hoan thanh design: navbar, slider, categories

---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/XsdmR2c/1.png",
+        title: 'SUMMER SALE',
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: 'f5fafd'
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/XsdmR2c/1.png",
+        title: 'WINTER SALE',
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: 'fcf1ed'
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/XsdmR2c/1.png",
+        title: 'POPULAR SALE',
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: 'fbf0f4'
+    }
+]
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://fakeimg.pl/350x200/?text=World&font=lobster",
+        title: "SHIRT STYLE"
+    },
+    {
+        id: 2,
+        img: "https://fakeimg.pl/350x200/?text=World&font=lobster",
+        title: "SHIRT STYLE"
+    },
+    {
+        id: 3,
+        img: "https://fakeimg.pl/350x200/?text=World&font=lobster",
+        title: "SHIRT STYLE"
+    }
+]
