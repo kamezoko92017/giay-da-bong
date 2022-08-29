b0e9e484d7a763d0a136c549f1dceb0819e0a9cc
albertbrasi <albertbrasi001@gmail.com>
2022-08-29 15:38:27 +0700

    Hoan thanh page: Product.jsx

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
 import ProductList from './pages/ProductList';
+import Product from './pages/Product';
 
 function App() {
   return (
-    <ProductList />
+    <Product />
   );
 }
 
