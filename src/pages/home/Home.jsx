d6e454e9c7652a6a667e9d7e41b693384181cafe
albertbrasi <albertbrasi001@gmail.com>
2022-09-14 20:16:02 +0700

    Xong phần User Information component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,18 @@ import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css";
 import { userData } from "../../dummyData";
+import WidgetLg from "../../components/widgetLg/widgetLg";
+import WidgetSm from "../../components/widgetSm/widgetSm";
 
 const Home = () => {
     return (
         <div className="home">
             <FeaturedInfo />
             <Chart data={userData} title="User Analytics" grid dataKey="Active User" />
+            <div className="homeWidgets">
+                <WidgetSm />
+                <WidgetLg />
+            </div>
         </div>
     )
 }
