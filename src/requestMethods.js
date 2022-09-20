d33e0bc2cfd573765f2752b88f7a88219fff90cf
albertbrasi <albertbrasi001@gmail.com>
2022-09-20 11:05:20 +0700

    hardcode TOKEN test deploy admin on Netlify

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
 const BASE_URL = "https://giay-da-bong.herokuapp.com/api/";
-// const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMGY3NGJkZjc5ZTU5ZWM3YmZkNTMwOCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MjY0NTUwMiwiZXhwIjoxNjYyOTA0NzAyfQ.-RJJbTR_MJ-A8jKjaeVB5TF60vpzprlfQvxdgGrL7_w";
-let TOKEN = "";
-if (JSON.parse(localStorage.getItem("persist:root"))) {
-    if (JSON.parse(localStorage.getItem("persist:root")).user) {
-        if (JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser) {
-            TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
-        }
-    }
-}
+const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMGY3NGJkZjc5ZTU5ZWM3YmZkNTMwOCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MzY0MDk2OSwiZXhwIjoxNjYzOTAwMTY5fQ.M0pbIatvEaUr-bfdPVdurk1iX3OfYqP2HNvZhN13HSY";
+// let TOKEN = "";
+// if (JSON.parse(localStorage.getItem("persist:root"))) {
+//     if (JSON.parse(localStorage.getItem("persist:root")).user) {
+//         if (JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser) {
+//             TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
+//         }
+//     }
+// }
 
 
 console.log("TOKEN: ", TOKEN);
